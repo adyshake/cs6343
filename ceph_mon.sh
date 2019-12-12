d94eb528c569b45d15f4381fe77586c229e99e2b
Adnan Shaikh <adnan.shaikh1806@gmail.com>
2019-12-12 17:46:03 -0500

    More changes

--- ceph_mon.sh
+++ ceph_mon.sh
@@ -18,9 +18,31 @@ ceph osd dump | grep 'replicated size'
 sudo mkdir /mnt/demo_cephfs
 sudo ceph-fuse /mnt/demo_cephfs
 
-rados -p demo_cephfs_data ls
+//----------------Beginning Test 1 - Check if balancing works---------------------
 
-ceph osd map demo_cephfs_data object1
+rados -p demo_cephfs_data ls
+//10000000001.00000000
+
+ceph osd map demo_cephfs_data 10000000001.00000000
+//osdmap e99 pool 'demo_cephfs_data' (3) object '10000000001.00000000' -> pg 3.c9e0747 (3.47) -> up ([1,2], p1) acting ([1,2], p1)
+
+ceph osd tree
+ID CLASS WEIGHT  TYPE NAME     STATUS REWEIGHT PRI-AFF 
+-1       0.17868 root default                          
+-3       0.06149     host mon1                         
+ 0   hdd 0.06149         osd.0     up  1.00000 1.00000 //mon1 pc1_vm2
+-5       0.06149     host osd1                         
+ 1   hdd 0.06149         osd.1   down        0 1.00000 //osd1 pc1_vm3
+-7       0.05569     host osd7                         
+ 2   hdd 0.05569         osd.2     up  1.00000 1.00000 //osd7 pc3_vm1
+
+Currently data is on pg 3.47 and its up on 1(pc1_vm3) and 2(pc3_vm1)
+
+//Immediately after brinf 3(pc3_vm1) down
+osdmap e102 pool 'demo_cephfs_data' (3) object '10000000001.00000000' -> pg 3.c9e0747 (3.47) -> up ([2], p2) acting ([2], p2)
+
+//After waiting for some time for rebalancing to occur
+osdmap e104 pool 'demo_cephfs_data' (3) object '10000000001.00000000' -> pg 3.c9e0747 (3.47) -> up ([2,0], p2) acting ([2,0], p2)
 
 //----------How placement group ID is generated-------------
 /*
